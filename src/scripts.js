7268cab5dbb5c19eac9942768d4a4bd739d2c913
Joseph Pearce <joseph@zotta.com>
2020-04-23 15:59:24 -0700

    add pull method for api that is now working.

--- src/scripts.js
+++ src/scripts.js
@@ -5,13 +5,7 @@ export class Genius {
   async getAPI(song) {
     try {
       let response = await fetch(`http://api.genius.com/search?q=${song}&access_token=${process.env.API_KEY}`);
-      // let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${song}&appid=${process.env.API_KEY2}`);
-      // https://api.discogs.com/artists/1?callback=callbackname
-      // let response = await fetch(`api.discogs.com/artists/1?q=${song}&access_token=${process.env.API_KEY3}`);
-
-      //response.hit.pimary_artist.name
-      // "name": "The Beatles",g
-
+    
 
       let jsonResponse;
       if (response.ok && response.status == 200) {
